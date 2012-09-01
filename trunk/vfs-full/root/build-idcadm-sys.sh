1ccf0a806e20e3f31998bfae166ed8a2d57f975c
rssn@163.com <rssn@163.com@bd62818b-e1c5-57c9-0423-abdc4f2e192c>
2012-09-01 06:34:18 +0000

    1. build-idcadm-sys.sh: label of root partition is fixed to "root-idcadm"; 2. build-system.sh: limitted the modification of "root=" to only the formats like "root=LABEL=xxx" and "root=/dev/[hs]d[a-z]", to avoid overwriting "root=/dev/ram0";

--- trunk/vfs-full/root/build-idcadm-sys.sh
+++ trunk/vfs-full/root/build-idcadm-sys.sh
@@ -2,24 +2,17 @@
 
 BUILD_SYSTEM_EXEC=""
 
-check_build_system_exec()
-{
-	local __path
-	for __path in . /usr/sbin /usr/bin /sbin /bin; do
-		if [ -x $__path/build-system.sh ]; then
-			BUILD_SYSTEM_EXEC="$__path/build-system.sh"
-			break
-		fi
-	done
-	if [ -z "$BUILD_SYSTEM_EXEC" ]; then
-		"*** No 'build-system.sh' found."
-		exit 1
+for __path in . /usr/sbin /usr/bin /sbin /bin; do
+	if [ -x $__path/build-system.sh ]; then
+		BUILD_SYSTEM_EXEC="$__path/build-system.sh"
+		break
 	fi
-	return 0
-}
-
-check_build_system_exec
+done
+if [ -z "$BUILD_SYSTEM_EXEC" ]; then
+	echo "*** No 'build-system.sh' found."
+	exit 1
+fi
 
 # --------------------- Modify or add your arguments here ---------------------
-exec $BUILD_SYSTEM_EXEC --root="30G,ext3," --swap="4G," --part3=",ext3,admdata,/adm" --fdisk "$@"
+exec $BUILD_SYSTEM_EXEC --root="30G,ext3,root-idcadm" --swap="4G," --part3=",ext3,admdata,/adm" --fdisk "$@"
 
