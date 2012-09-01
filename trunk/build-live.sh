bf0727ca95d0d9313d0500fbed2aa71c53439b1d
rssn@163.com <rssn@163.com@bd62818b-e1c5-57c9-0423-abdc4f2e192c>
2012-09-01 05:44:28 +0000

    1. build-live.sh: added construction for "/dev" directory; 2. linux-2.6.32.8-liveusb/.config -> config-2.6.32.8-liveusb;

--- trunk/build-live.sh
+++ trunk/build-live.sh
@@ -1,53 +1,87 @@
 #!/bin/sh -e
 
-VFS_SOURCE=vfs-full
-VFS_IMAGE=vfs-full.gz
+PWD=`pwd`
+
+VFS_SOURCE=$PWD/vfs-full
+VFS_IMAGE=$PWD/vfs-full.gz
+KERNEL_SOURCE=$PWD/linux-2.6.32.8-liveusb
+
 KERNEL_VERSION=2.6.32.8-liveusb
-KERNEL_SOURCE=linux-2.6.32.8-liveusb
+
 
 do_kernel_make()
 {
+	[ ! -e $KERNEL_SOURCE/.config ] && cp -f config-$KERNEL_VERSION $KERNEL_SOURCE/.config
 	make -C $KERNEL_SOURCE
+	cp -vf $KERNEL_SOURCE/.config config-$KERNEL_VERSION
 	make install -C $KERNEL_SOURCE
 	depmod $KERNEL_VERSION
+
 }
 
 do_vfs_make()
 {
-	local __vfs_mp="__d"
-	local __vfs_lo="__t"
+	local __vfs_mnt=$PWD/__d
+	local __vfs_loop=$PWD/__t
 	
 	do_kernel_make
 	
 	mkdir -p boot
 	cp -af /boot/*-$KERNEL_VERSION boot/
-	mkdir -p $__vfs_mp
-	dd if=/dev/zero of=$__vfs_lo bs=1M count=64
-	echo y | mkfs.ext2 -I128 -L vfs-full $__vfs_lo
-	mount $__vfs_lo $__vfs_mp -o loop
-	cp -af $VFS_SOURCE/* $__vfs_mp/
-	cp -af /lib/modules/$KERNEL_VERSION $__vfs_mp/lib/modules/
-	umount $__vfs_mp
-	rmdir $__vfs_mp
-	gzip -c $__vfs_lo > $VFS_IMAGE
-	rm -f $__vfs_lo
+	mkdir -p $__vfs_mnt
+	dd if=/dev/zero of=$__vfs_loop bs=1M count=64
+	echo y | mkfs.ext2 -I128 -L vfs-full $__vfs_loop
+	mount $__vfs_loop $__vfs_mnt -o loop
+	(
+		cd $VFS_SOURCE
+		tar -c --exclude=.svn * | tar -xv -C $__vfs_mnt
+		
+		# Build /dev directory
+		mkdir -p $__vfs_mnt/dev
+		cd $__vfs_mnt/dev
+		
+		mknod ram0 b 1 0
+		mknod ram1 b 1 1
+		mknod ram2 b 1 2
+		ln -s ram0 ramdisk
+		ln -s ram1 ram
+		mknod console c 5 1
+		mknod tty c 5 0
+		mknod tty0 c 4 0
+		mknod tty1 c 4 1
+		mknod tty2 c 4 2
+		mknod tty3 c 4 3
+		mknod tty4 c 4 4
+		mknod tty5 c 4 5
+		mknod null c 1 3
+		mknod ptmx c 5 2
+		mknod urandom c 1 9
+		mknod zero c 1 5
+		
+		mkdir -p pts shm
+	)
+	cp -auvf /lib/modules/$KERNEL_VERSION $__vfs_mnt/lib/modules/
+	umount $__vfs_mnt
+	rmdir $__vfs_mnt
+	gzip -c $__vfs_loop > $VFS_IMAGE
+	rm -f $__vfs_loop
 }
 
 do_install()
 {
 	[ -z "$1" ] && { echo "*** No flash disk partition specified."; exit 1; }
-
+	
 	local __flash_dev="$1"
-	local __flash_mp="__u"
+	local __flash_mnt=$PWD/__u
 	
 	do_vfs_make
 	
-	mkdir -p $__flash_mp
-	mount $__flash_dev $__flash_mp
-	[ ! -e $__flash_mp/boot ] && mkdir $__flash_mp/boot
-	cp -af /boot/*-$KERNEL_VERSION $VFS_IMAGE $__flash_mp/boot/
-	umount $__flash_mp
-	rmdir $__flash_mp
+	mkdir -p $__flash_mnt
+	mount $__flash_dev $__flash_mnt
+	[ ! -e $__flash_mnt/boot ] && mkdir $__flash_mnt/boot
+	cp -af /boot/*-$KERNEL_VERSION $VFS_IMAGE $__flash_mnt/boot/
+	umount $__flash_mnt
+	rmdir $__flash_mnt
 }
 
 do_clean()
