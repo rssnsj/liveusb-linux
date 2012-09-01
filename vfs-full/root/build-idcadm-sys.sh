1f33dec101859012fe6c18779179ff666e3cfbf9
rssn@163.com <rssn@163.com@bd62818b-e1c5-57c9-0423-abdc4f2e192c>
2012-09-01 06:45:09 +0000

    1. vfs-full/etc/rc.local: Add support for interface address setting with both "address/bits" and "address/netmask"; 2. build-idcadm-sys.sh: Simplified the script path checking method;

--- vfs-full/root/build-idcadm-sys.sh
+++ vfs-full/root/build-idcadm-sys.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
 
-BUILD_SYSTEM_EXEC=""
+BUILD_SYSTEM_EXEC="/usr/sbin/build-system.sh"
 
-for __path in . /usr/sbin /usr/bin /sbin /bin; do
-	if [ -x $__path/build-system.sh ]; then
-		BUILD_SYSTEM_EXEC="$__path/build-system.sh"
-		break
-	fi
-done
-if [ -z "$BUILD_SYSTEM_EXEC" ]; then
-	echo "*** No 'build-system.sh' found."
-	exit 1
-fi
+[ -e "$BUILD_SYSTEM_EXEC" ] || BUILD_SYSTEM_EXEC=./build-system.sh
 
 # --------------------- Modify or add your arguments here ---------------------
-exec $BUILD_SYSTEM_EXEC --root="30G,ext3,root-idcadm" --swap="4G," --part3=",ext3,admdata,/adm" --fdisk "$@"
+exec $BUILD_SYSTEM_EXEC --root="30G,ext3,idcadm-root" --swap="4G," --part3=",ext3,admdata,/adm" --fdisk "$@"
 
