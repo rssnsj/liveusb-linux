f39d939c78770f316c7ac2a7a0e601ecfaf35d64
Justin Liu <rssnsj@gmail.com>
2023-06-15 00:25:42 +0800

    Update kernel to 5.4

--- src/build-live.sh
+++ src/build-live.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-KERNEL_VERSION=4.20
+KERNEL_VERSION=5.4
 KERNEL_RELEASE=$KERNEL_VERSION-liveusb
-KERNEL_DOWNLOAD_URL="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.20.tar.xz"
+KERNEL_DOWNLOAD_URL="https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.4.tar.xz"
 
 VFS_SOURCE_DIR=rootfs
 INSTALL_DIR=bin
